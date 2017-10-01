8cddf5a3e9a5f9d4adba21bb968d4ef1ff84adc6
Timothy T Mugadza <mugadzatt001@gmail.com>
2017-10-01 17:11:47 +0200

    Header

--- libftasm.h
+++ libftasm.h
@@ -1,38 +1,7 @@
-/* ************************************************************************** */
-
-/*                                                                            */
-
-/*                                                        :::      ::::::::   */
-
-/*   libftasm.h                                         :+:      :+:    :+:   */
-
-/*                                                    +:+ +:+         +:+     */
-
-/*   By: rbaum <rbaum@student.42.fr>                +#+  +:+       +#+        */
-
-/*                                                +#+#+#+#+#+   +#+           */
-
-/*   Created: 2015/03/26 22:38:54 by rbaum             #+#    #+#             */
-
-/*   Updated: 2015/03/31 00:40:59 by rbaum            ###   ########.fr       */
-
-/*                                                                            */
-
-/* ************************************************************************** */
-
-
-
 #ifndef LIBFTASM_H
-
 # define LIBFTASM_H
-
 # include <unistd.h>
-
 # include <stdlib.h>
-
-
-
-
 
 void		ft_bzero(void *s, size_t n);
 
@@ -82,28 +51,4 @@ void		ft_cat(int fd);
 
 
 
-
-
-//char		*ft_strnew(size_t size);
-
-//int			ft_isupper(int c);
-
-//int			ft_islower(int c);
-
-
-
-//int			ft_putserr(const char *s);
-
-//int			ft_putsfd(int fd, const char *s);
-
-//void*		ft_memalloc(size_t n);
-
-//void		ft_strclr(char *s);
-
-
-
-
-
 #endif
-
-['../ft_org.py', '../Libasm-master/inc/libftasm.h', ';']
